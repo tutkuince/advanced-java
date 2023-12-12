25b64431672720a42adc5ae0d572d65cd6b91ac6
Tutku Ince <tutku.ince@outlook.com>
2023-12-12 20:54:12 +0300

    information has been added to ex2

--- src/main/java/io/tince/advanced/java/lambda/expressions/exercises/Exercise02.java
+++ src/main/java/io/tince/advanced/java/lambda/expressions/exercises/Exercise02.java
@@ -3,6 +3,14 @@ package io.tince.advanced.java.lambda.expressions.exercises;
 import java.util.function.Supplier;
 
 public class Exercise02 {
+
+    /**
+     * In main() invoke the supplier() method; in supplier() do the following:
+     *
+     * a) Using a lambda expression, implement the Retrievable interface (typed for Integer). The relevant method just returns 77. Invoke the relevant method.
+     *
+     * b) Using a lambda expression, implement 2a using a Supplier.
+     * */
     public static void main(String[] args) {
         Retrievable<Integer> retrievable = () -> {return 77;};
         System.out.println("Retrievable: " + retrievable.retrieve());
