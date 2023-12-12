167f1d4db49fae188710279e6f9f2cb6086449f0
Tutku Ince <tutku.ince@outlook.com>
2023-12-12 21:17:00 +0300

    Person

---
+++ src/main/java/io/tince/advanced/java/lambda/expressions/exercises/part02/Person.java
@@ -0,0 +1,46 @@
+package io.tince.advanced.java.lambda.expressions.exercises.part02;
+
+public class Person {
+    private Integer age;
+    private String name;
+    private Double height;
+
+    public Person(String name, Integer age, Double height) {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", height=" + height +
+                '}';
+    }
+}
