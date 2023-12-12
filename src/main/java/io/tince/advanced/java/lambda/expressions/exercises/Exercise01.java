4f491fc1a19cbefed8df9d6c24aa63ef8c243429
Tutku Ince <tutku.ince@outlook.com>
2023-12-12 20:21:39 +0300

    exercise 1

--- src/main/java/io/tince/advanced/java/lambda/expressions/exercises/Exercise01.java
+++ src/main/java/io/tince/advanced/java/lambda/expressions/exercises/Exercise01.java
@@ -1,5 +1,7 @@
 package io.tince.advanced.java.lambda.expressions.exercises;
 
+import java.util.function.Consumer;
+
 public class Exercise01 {
 
     // Using a lambda expression, implement the Printable interface (typed for String).
@@ -7,9 +9,14 @@ public class Exercise01 {
     // b) Using both a lambda expression and a method reference, implement 1a using a Consumer.
 
     public static void main(String[] args) {
-        Printable<String> printable1 = (s) -> System.out.println(s);
-
+        Printable<String> printable1 = s -> System.out.println(s);
         printable1.print("Printable lambda");
+
+        Consumer<String> stringConsumer = s -> System.out.println(s);
+        stringConsumer.accept("Consumer lambda");
+
+        Consumer<String> stringConsumerMR = System.out::println;
+        stringConsumerMR.accept("Consumer method reference");
     }
 }
 
