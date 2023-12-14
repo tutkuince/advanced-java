3f931a626db165106c157f277bbb50cd45258583
Tutku Ince <tutku.ince@outlook.com>
2023-12-14 22:28:06 +0300

    doReduce1

--- src/main/java/io/tince/advanced/java/streams/operations/terminal/TerminalOperations.java
+++ src/main/java/io/tince/advanced/java/streams/operations/terminal/TerminalOperations.java
@@ -24,6 +24,34 @@ public class TerminalOperations {
         System.out.println(word);
     }
 
+    public static void doReduce1(){
+
+        // The reduce() method combines a stream into a single object.
+        // It is a reduction, which means it processes all elements.
+        // The most common way of doing a reduction is to start with
+        // an initial value and keep merging it with the next value.
+
+        // T reduce(T identity, BinaryOperator<T> accumulator)
+        //      BinaryOperator<T> functional method:
+        //          T apply(T, T);
+        // The "identity" is the initial value of the reduction and also
+        // what is returned if the stream is empty. This means that there
+        // will always be a result and thus Optional is not the return type
+        // (on this version of reduce()).
+        // The "accumulator" combines the current result with the
+        // current value in the stream.
+        String name = Stream.of("s", "e", "a", "n")
+//                        . filter(s -> s.length()>2)
+//                       .reduce("nothing", (s, c) -> s + c);
+                .reduce("", (s, c) -> s + c);
+        System.out.println(name);// sean
+
+        Integer product = Stream.of(2,3,4)
+                .reduce(1, (a, b) -> a * b);
+        System.out.println(product);// 24
+
+    }
+
     public static void doReduce2(){
 
         // Optional<T> reduce(BinaryOperator<T> accumulator)
