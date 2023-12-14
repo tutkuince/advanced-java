317b54d0ff09e8ae866b1d34dc19b78de16054c1
Tutku Ince <tutku.ince@outlook.com>
2023-12-14 22:27:20 +0300

    doReduce3

--- src/main/java/io/tince/advanced/java/streams/operations/terminal/TerminalOperations.java
+++ src/main/java/io/tince/advanced/java/streams/operations/terminal/TerminalOperations.java
@@ -22,4 +22,26 @@ public class TerminalOperations {
                         (stringBuilder, stringBuilder2) -> stringBuilder.append(stringBuilder2));
         System.out.println(word);
     }
+
+    public static void doReduce3(){
+
+        // <U> U reduce (U identity,
+        //               BiFunction accumulator,
+        //               BinaryOperator combiner)
+        // We use this version when we are dealing with different types,
+        // allowing us to create intermediate reductions and then combine
+        // them at the end. This is useful when working with parallel
+        // streams - the streams can be decomposed and reassembled by
+        // separate threads. For example, if we wanted to count the length
+        // of four 1000-character strings, the first 2 values and the last
+        // two values could be calculated independently. The intermediate
+        // results (2000) would then be combined into a final value (4000).
+        // Example: we want to count the number of characters in each String
+        Stream<String> stream = Stream.of("car", "bus", "train", "aeroplane");
+        int length = stream.reduce( 0,  // identity
+                (n, str) -> n + str.length(), // n is Integer
+                (n1, n2) -> n1 + n2); // both are Integers
+        System.out.println(length);// 20
+    }
+
 }
