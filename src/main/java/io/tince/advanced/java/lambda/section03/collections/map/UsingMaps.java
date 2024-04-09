4d16ad17227eb3b0aa40a1635d633db2e57565af
Tutku Ince <tutku.ince@outlook.com>
2024-04-09 16:21:11 +0300

    UsingMaps

---
+++ src/main/java/io/tince/advanced/java/lambda/section03/collections/map/UsingMaps.java
@@ -0,0 +1,31 @@
+package io.tince.advanced.java.lambda.section03.collections.map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class UsingMaps {
+    public static void main(String[] args) {
+        Map<String, Integer> mapTree = new TreeMap<>(); // sorted by keys
+        mapTree.put("John", 18);
+        mapTree.put("Mary", 23);
+        mapTree.put("Chris", 62);
+        System.out.println(mapTree.containsKey("John"));    // true
+        System.out.println(mapTree.containsValue(18));      // true
+        System.out.println(mapTree.isEmpty());              // false
+        System.out.println(mapTree.get("John"));            // 18
+
+        for (String name :mapTree.keySet()) {
+            System.out.println(name);
+        }
+
+        for (Integer age : mapTree.values()) {
+            System.out.println(age);
+        }
+
+        System.out.println(mapTree.containsValue("Paul"));  // false
+        System.out.println(mapTree.containsValue(23));      // true
+        System.out.println(mapTree.size());                 // 3
+        mapTree.clear();
+        System.out.println(mapTree.size());                 // 0
+    }
+}
