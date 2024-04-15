47877bcd11c6cd3d57b96dd7cafa33524b42e37e
Tutku Ince <tutku.ince@outlook.com>
2024-04-15 14:58:05 +0300

    priorityQueue natural ordering

--- src/main/java/io/tince/advanced/java/lambda/section03/collections/queue/UsingQueues.java
+++ src/main/java/io/tince/advanced/java/lambda/section03/collections/queue/UsingQueues.java
@@ -1,11 +1,35 @@
 package io.tince.advanced.java.lambda.section03.collections.queue;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class UsingQueues {
     public static void main(String[] args) {
         linkedListQueue();
+        priorityQueueNaturalOrdering();
+    }
+
+    private static void priorityQueueNaturalOrdering() {
+        // Natural Ordering
+        Queue<String> names = new PriorityQueue<>();    // alphabetic ordering
+        names.add("V");
+        names.add("P");
+        names.add("A");
+        Iterator<String> iteratorNames = names.iterator();
+        while (iteratorNames.hasNext()) {
+            System.out.print(names.poll() + " "); // A P V
+        }
+
+        Queue<Integer> numbers = new PriorityQueue<>(); // numeric ordering
+        numbers.add(11);
+        numbers.add(5);
+        numbers.add(2);
+        Iterator<Integer> iteratorNumbers = numbers.iterator();
+        while (iteratorNumbers.hasNext()) {
+            System.out.print(numbers.poll() + " "); // 2 5 11
+        }
     }
 
     private static void linkedListQueue() {
