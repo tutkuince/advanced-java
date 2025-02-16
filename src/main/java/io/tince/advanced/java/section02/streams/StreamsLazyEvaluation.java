391013bb0c8a493b559e80baf62afe244c3582ce
Tutku Ince <tutku.ince@outlook.com>
2025-02-16 18:17:50 +0300

    Lazy Evaluation

--- src/main/java/io/tince/advanced/java/section02/streams/StreamsLazyEvaluation.java
+++ src/main/java/io/tince/advanced/java/section02/streams/StreamsLazyEvaluation.java
@@ -5,6 +5,14 @@ import java.util.stream.Stream;
 public class StreamsLazyEvaluation {
 
     public static void main(String[] args) {
+        // Lazy Evaluation
         Stream<Integer> integerStream = Stream.of(10, 11, 12, 13, 14);
+
+        Stream<Integer> filteredIntegerStream = integerStream.filter(i -> {
+            System.out.println("Filtering integer");
+            return i % 2 == 0;
+        });
+
+        System.out.println("Count is: " + filteredIntegerStream.count());
     }
 }
